55b71e4f74e7c84c594d8e5753889171bcc7c5ed
LeandervanAarde <200211@virtualwindow.co.za>
2022-04-01 23:29:09 +0200

    PLayer timeline has been fixed
    Player timeline will take in one stat from players now with 5 options.

--- src/Components/Playertimeline.js
+++ src/Components/Playertimeline.js
@@ -57,26 +57,15 @@ const Playertimeline = () => {
         borderwidth: 1
 
     };
-            
+
 
     return (
         <Row className='timeline-row'>
-            <Col className='col-12 col-lg-4 input-container' id='input'>
+            <Col className='col-12 col-lg-4 input-container ms-5' id='input'>
                 <InputGroup className="mb-3 mt-4 input" >
                     <FormControl
                         placeholder="Player Name"
                         aria-label="Player Name"
-                        aria-describedby="basic-addon1"
-                    />
-                </InputGroup>
-
-            </Col>
-
-            <Col className='col-12 col-lg-4 offset-lg-4  input-container'>
-                <InputGroup className="mb-3 mt-4 input">
-                    <FormControl
-                        placeholder="season eg. 2020/2021"
-                        aria-label="Season"
                         aria-describedby="basic-addon1"
                     />
                 </InputGroup>
@@ -99,91 +88,55 @@ const Playertimeline = () => {
 
             </Col>
 
-            <Col className='col-12 col-lg-3 input-container'>
-
-                <DropdownButton id="dropdown-custom" title="Player Statistic #2" className="mb-3 mt-4 col-12 input drop">
-                    <Dropdown.Header className='dropdown-head'>Choose player statistic #2 </Dropdown.Header>
-                    <Dropdown.Item href="#/action-2">Shots</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Goals</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Shot Pct</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Assists</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Powerplay Goals</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Points</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Penalties (min)</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Plus Minus</Dropdown.Item>
-                </DropdownButton>
+            <Col className='col-12 col-lg-4 offset input-container'>
+                <InputGroup className="mb-3 mt-4 input">
+                    <FormControl
+                        placeholder="season eg. 2020/2021"
+                        aria-label="Season"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
 
             </Col>
 
-            <Col className='col-12 col-lg-3 input-container'>
-
-                <DropdownButton id="dropdown-custom" title="Player Statistic #3" className="mb-3 mt-4 col-12 input drop">
-                    <Dropdown.Header className='dropdown-head'>Choose player statistic #3 </Dropdown.Header>
-                    <Dropdown.Item href="#/action-2">Shots</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Goals</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Shot Pct</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Assists</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Powerplay Goals</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Points</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Penalties (min)</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Plus Minus</Dropdown.Item>
-                </DropdownButton>
-
-            </Col>
-
-            <Col className='col-12 col-lg-3 input-container'>
-
-                <DropdownButton id="dropdown-custom" title="Player Statistic #4" className="mb-3 mt-4 col-12 input drop">
-                    <Dropdown.Header className='dropdown-head'>Choose player statistic #4 </Dropdown.Header>
-                    <Dropdown.Item href="#/action-2">Shots</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Goals</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Shot Pct</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Assists</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Powerplay Goals</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Points</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Penalties (min)</Dropdown.Item>
-                    <Dropdown.Item href="#/action-1">Plus Minus</Dropdown.Item>
-                </DropdownButton>
-
-            </Col>
 
             <Col className='col-12 chart-container mt-5 mb-5'>
                 <div className='timeline col-12 mb-2'>
-                        <Line data ={lineData}
-                        
+                    <Line data={lineData}
+
                         options={{
-                      
-                        elements: {
-                            line: {
-                                borderWidth: 9,
-                                borderColor: ["rgb(200 ,16, 46)", "rgb(30, 30, 148)", "rgb(95, 3, 114)", "rgb(197, 197, 197)"],
-                            }
-                        },
-                        maintainAspectRatio: false,
-                        
-                        plugins:{
-                            decimation:{
-                                enabled: true, 
+
+                            elements: {
+                                line: {
+                                    borderWidth: 9,
+                                    borderColor: ["rgb(200 ,16, 46)", "rgb(30, 30, 148)", "rgb(95, 3, 114)", "rgb(197, 197, 197)"],
+                                }
                             },
-                           
-                            legend:{
-                                position: "bottom", 
-                               labels:{
-                               
-                                padding: 30, 
-                                boxWidth: 30,
-                                boxHeight: 20, 
-                                
-                               
-                               }
-                            }
-                        }, 
+                            maintainAspectRatio: false,
+
+                            plugins: {
+                                decimation: {
+                                    enabled: true,
+                                },
+
+                                legend: {
+                                    position: "bottom",
+                                    labels: {
+
+                                        padding: 30,
+                                        boxWidth: 30,
+                                        boxHeight: 20,
 
 
-                    
-                    }}
-                        
-                        />
+                                    }
+                                }
+                            },
+
+
+
+                        }}
+
+                    />
                 </div>
             </Col>
 
