46dac915c5cbeebcb1a95c29b412e04984fd4b98
LeandervanAarde <200211@virtualwindow.co.za>
2022-04-10 17:57:07 +0200

    Logos showing
    Logos are now showing on the scores

--- src/Components/Scores.js
+++ src/Components/Scores.js
@@ -23,8 +23,28 @@ const Scores = () => {
             // console.log(data);
             const game1Arr = [];
             // console.log(data);
+            let teamLogo;
+            let teamAwayLogo;
             for(let i = 0; i < data.length; i++){
+          
+                for(let j = 0; j < logos["NHL_Logos"].length; j++){
+                    console.log("Hello");
+                    if(logos["NHL_Logos"][j]["name"].includes(data[i].teams.home.team.name)){
+                        teamLogo = logos["NHL_Logos"][j]["url"];
+                       
+                        break;
+                    }
+                    console.log("home logo found");
+                }
+                for(let j = 0; j <logos["NHL_Logos"].length; j++){
+                    if( logos["NHL_Logos"][j]["name"].includes(data[i].teams.away.team.name)){
+                        teamAwayLogo = logos["NHL_Logos"][j]["url"];
+                       
+                        break;
 
+                    }
+                    console.log("Away logo found");
+                }
                 // let teamLogo= logos["NHL_Logos"].filter(element =>{
                 //    return element["name"] == data[i].teams.home.team.name;  
 
@@ -34,8 +54,7 @@ const Scores = () => {
                 //     return element["name"] == data[i].teams.away.team.name;  
  
                 //  });
-                let teamLogo = logos["NHL_Logos"][0];
-                let teamAwayLogo = logos["NHL_Logos"][2];
+              
 
                game1Arr.push({
                    //push home and away teams to the game1 array in order to get both scores
@@ -45,15 +64,15 @@ const Scores = () => {
                     idAway: data[i].teams.away.team.id,
                     teamNameAway: data[i].teams.away.team.name,
                     scoreAway: data[i].teams.away.score,
-                    Logo: teamLogo["url"],
-                    logoAway: teamAwayLogo["url"]
+                    Logo: teamLogo,
+                    logoAway: teamAwayLogo
                 });
             }
             //set game one to a UseStae
             setGameOne(game1Arr);
             //Map the data
-             const scoreOne = game1Arr.map((item) => <Scoreitem key={item.id} teamName ={item.teamName} score={item.score} teamNameAway = {item.teamNameAway} scoreAway={item.scoreAway} logoOne = {item.Logo} logoTwo = {item.LogoAway}></Scoreitem>);
-             const scoreTwo = game1Arr.map((item) => <Scoreitem key={item.id} teamName ={item.teamName} score={item.score} teamNameAway = {item.teamNameAway} scoreAway={item.scoreAway} logoOne = {item.Logo} logoTwo = {item.LogoAway}></Scoreitem>);
+             const scoreOne = game1Arr.map((item) => <Scoreitem key={item.id} teamName ={item.teamName} score={item.score} teamNameAway = {item.teamNameAway} scoreAway={item.scoreAway} logoOne = {item.Logo} logoTwo = {item.logoAway}></Scoreitem>);
+             const scoreTwo = game1Arr.map((item) => <Scoreitem key={item.id} teamName ={item.teamName} score={item.score} teamNameAway = {item.teamNameAway} scoreAway={item.scoreAway} logoOne = {item.Logo} logoTwo = {item.logoAway}></Scoreitem>);
              //set the final score
              setfirstScore(scoreOne);
              setSecondScore(scoreTwo);
--- src/Components/scoreItem.js
+++ src/Components/scoreItem.js
@@ -3,15 +3,30 @@ import React from 'react';
 const scoreItem = (props) => {
     return (
         <>
-            <div className='circle '>
-                <img src={props.logoOne}/>
+            <div className='circle ' 
+            
+            style={{  
+                backgroundImage: "url(" + props.logoOne + ")",
+                backgroundPosition: 'center',
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat'
+              }}
+            >
+                {/* <img className='logo' src={props.logoOne}/> */}
             </div>
           
             <div className='text' id='right' key={props.id}><h3>{props.score}</h3></div>
             <div className='text' id='right' key={props.idAway}><h3>{props.scoreAway}</h3></div> 
 
-             <div className='circle '>
-
+             <div className ='circle '
+                 style={{  
+                    backgroundImage: "url(" + props.logoTwo + ")",
+                    backgroundPosition: 'center',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat'
+                  }}
+             >
+          
             </div>    
         </>
     );
